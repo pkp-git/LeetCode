d5ae9c1a977a9c72619d016c367edcf19b1c6943
pkp-git <144834679+pkp-git@users.noreply.github.com>
2024-09-15 23:53:44 +0530

    Time: 193 ms (13.6%), Space: 16.7 MB (40.18%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,38 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = ""
+        count = 0
+        maxlen = 0
+        for i in range(0,len(s)):
+            if s[i] not in chars:
+                count += 1
+                chars += s[i]
+                print("s[i]/.",s[i], "./ if stat: ", count, "char:/.",chars,"./")
+            else:
+                print("begin else: s[i]/.",s[i],",/")
+                chars = chars[::-1]
+                print(chars)
+                idx = chars.find(s[i])
+                chars = s[i]+chars[:idx]
+                print(chars[:idx])
+                chars = chars[::-1]
+                count = len(chars)
+                print(idx, chars)
+                print("char:/.",chars,"./")
+            maxlen = max(count,maxlen)
+            print(maxlen)
+        return maxlen
+
+'''
+                count = 1 
+                for j in chars:
+                    if s[i] == j:
+                        change = s[i]
+                if chars[-1] == change:
+                    chars = chars[-1]
+                else:
+                    chars = chars[-1]+change
+                    count = 2
+'''
+
+        
