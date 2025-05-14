7a050f260a2558cdf44fdaa7facc5e1379ef7f43
PK <144834679+pkp-git@users.noreply.github.com>
2025-05-14 20:51:23 +0530

    Time: 0 ms (100%), Space: 19.6 MB (18.47%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d = {}
+
+        for i in nums:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] = d[i]+1
+
+        for i in d:
+            if d[i] == 1:
+                return i
